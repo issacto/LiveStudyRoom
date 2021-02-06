274ee1ca88d746a07a47bdddaee45ac5a38b8856
Issac To <issacto606@gmail.com>
2021-02-06 21:32:32 +0000

    Finalize videochat

--- src/Components/VideoChat/VideoChat.js
+++ src/Components/VideoChat/VideoChat.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useCallback } from 'react';
 import Lobby from './Lobby';
 import Room from './Room';
---
+++ src/Screens/VideoPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import VideoChat from "../Components/VideoChat/VideoChat.js";
+
+
+
+class AboutPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+      
+      
+  return (
+      <VideoChat/>
+    );
+  }
+}
+export default AboutPage;
