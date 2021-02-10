194c2b339b11164789851902194454cb61312347
Issac To <issacto606@gmail.com>
2021-02-10 19:49:47 +0000

    make it better

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const { videoToken } = require('./tokens');
+var cors = require('cors')
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
+app.use(cors());
 
 const sendTokenResponse = (token, res) => {
   res.set('Content-Type', 'application/json');
